a4db7545d67421422d2741f2ae88bfea8841c429
honeytteok <gogo101525@gmail.com>
2023-03-14 17:12:57 +0900

    첫커밋

---
+++ boost/main.js
@@ -0,0 +1,7 @@
+const toogleBtn = document.querySelector('.navbar_toogleBtn');
+const menu = document.querySelector('.navbar_menu');
+const icons = document.querySelector('.navbar_icons');
+
+toogleBtn.addEventListener('click', () => {});
+       menu.classList.toggle('active');
+       menu.classList.toggle('active');
